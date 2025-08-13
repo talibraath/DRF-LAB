2458743716a6d148a0a80a9fed25080280fe1039
Talib Husain <talib.raath@gmail.com>
2025-08-13 18:27:14 +0500

    Viewsets

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import ProductList, ProductListCreateView,ProductDetailView
+from .views import ProductList, ProductListCreateView,ProductDetailView, ProductViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet, basename='product')
 
 urlpatterns = [
     # path('products/', ProductList.as_view(), name='product-list'),
     # path('products/', ProductListCreateView.as_view(), name='product-list-create'),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    # path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
 
-]
+] + router.urls
--- products/views.py
+++ products/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductModelSerializer, ProductCustomSerializer
 from rest_framework import mixins, generics
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 class ProductList(APIView):
@@ -36,4 +38,20 @@ class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductModelSerializer
 
-    
+class ProductViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        queryset = Product.objects.all()
+        serializer = ProductModelSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductModelSerializer(product)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = ProductModelSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
