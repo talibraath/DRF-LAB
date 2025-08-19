5bd768ecc3d999c76c943fb2c3331ed2a57e616f
Talib Husain <talib.raath@gmail.com>
2025-08-19 11:39:12 +0500

    Throtling added

--- drflab/settings.py
+++ drflab/settings.py
@@ -45,6 +45,16 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 5,
+
+  "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "10/min",     
+        "user": "10/min",   
+    },
 }
 
 MIDDLEWARE = [
